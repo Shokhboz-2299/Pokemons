9ab59a487e124806131a582de80519aab7322b96
Shokhboz-2299 <shokhbozkaumutov@gmail.com>
2021-10-03 18:12:17 +0500

    Add pokemons style

--- js/pokemons.js
+++ js/pokemons.js
@@ -4628,13 +4628,13 @@ var elChiqar =$_(".chiqar-js");
 
 for (var i = 0; i < pokemons.length; i++) {
 	var list = document.createElement("li");
-list.setAttribute("class", "");
+list.setAttribute("class", "col-sm-4 col-md-3 col-lg-2 text-center p-3 mx-2 mb-2 border border-2 border-success rounded bg-light");
 
 var image = document.createElement("img");
 image.src = pokemons[i].img;
 list.append(image);
 
-var ism = document.createElement("h3");
+var ism = document.createElement("h4");
 ism.textContent = pokemons[i].name;
 list.append(ism);
 
